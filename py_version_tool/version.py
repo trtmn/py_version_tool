fe4d593bcabc6c7ff1c53f5ef37e5270ad1cd115
Matt Troutman <git@trtmn.com>
2023-11-02 18:06:48 -0500

    Update version.py
    added doc strings for better usability

--- py_version_tool/version.py
+++ py_version_tool/version.py
@@ -1,9 +1,14 @@
 import json
 import os
 
-
-# Function to create the version.json file with initial values if it doesn't exist
 def _create_version_file(filename='version.json'):
+    """
+    Create the version.json file with initial values if it doesn't exist.
+
+    Args:
+        filename (str): The name of the version JSON file.
+
+    """
     try:
         with open(filename, 'r') as file:
             pass  # File already exists
@@ -13,36 +18,65 @@ def _create_version_file(filename='version.json'):
         with open(filename, 'w') as file:
             json.dump(initial_version, file, indent=4)
 
-
-# Function to read the version information from the JSON file
 def _read_version(filename='version.json'):
+    """
+    Read the version information from the JSON file.
+
+    Args:
+        filename (str): The name of the version JSON file.
+
+    Returns:
+        dict: A dictionary containing major, minor, and patch version components.
+
+    """
     _create_version_file(filename)  # Check and create the file if it doesn't exist
 
     with open(filename, 'r') as file:
         return json.load(file)
 
-
-# Function to write the version information to the JSON file and update setup.py
 def _write_version(data, setup_py_filename='setup.py', filename='version.json'):
+    """
+    Write the version information to the JSON file and update setup.py.
+
+    Args:
+        data (dict): A dictionary containing major, minor, and patch version components.
+        setup_py_filename (str): The name of the setup.py file.
+        filename (str): The name of the version JSON file.
+
+    """
     with open(filename, 'w') as file:
         json.dump(data, file, indent=4)
 
     # Update setup.py with the new version
     update_setup_py_version(setup_py_filename, data)
 
-
-# Function to convert version dictionary to human-readable format
 def version_to_string(version_dict=None):
+    """
+    Convert version dictionary to a human-readable format.
+
+    Args:
+        version_dict (dict, optional): A dictionary containing major, minor, and patch version components.
+
+    Returns:
+        str: A human-readable version string in the format "major.minor.patch".
+
+    """
     if version_dict is None:
         version_dict = _read_version()  # Get the dictionary from _read_version
 
     return f"{version_dict['major']}.{version_dict['minor']}.{version_dict['patch']}"
 
-
-# Function to update setup.py with the new version
 def update_setup_py_version(setup_py_filename, version_data):
+    """
+    Update the version in the setup.py file.
+
+    Args:
+        setup_py_filename (str): The name of the setup.py file.
+        version_data (dict): A dictionary containing major, minor, and patch version components.
+
+    """
     if not os.path.exists(setup_py_filename):
-        # print(f"{setup_py_filename} does not exist.")
+        print(f"{setup_py_filename} does not exist.")
         return
 
     with open(setup_py_filename, 'r') as file:
@@ -56,9 +90,14 @@ def update_setup_py_version(setup_py_filename, version_data):
     with open(setup_py_filename, 'w') as file:
         file.writelines(setup_py_contents)
 
-
-# Function to increment the version components
 def increment_version(component):
+    """
+    Increment the specified version component (major, minor, or patch).
+
+    Args:
+        component (str): The version component to increment: "major", "minor", or "patch".
+
+    """
     data = _read_version()  # Read the initial version values
 
     if component == "major":
@@ -73,9 +112,15 @@ def increment_version(component):
 
     _write_version(data)  # Save the updated version back to the JSON file
 
-
-# Function to set version based on a version string
 def set_version(version_string, filename='version.json'):
+    """
+    Set the version based on a version string in the format "major.minor.patch".
+
+    Args:
+        version_string (str): The version string to set.
+        filename (str): The name of the version JSON file.
+
+    """
     data = _read_version(filename)  # Read the initial version values
 
     major, minor, patch = map(int, version_string.split('.'))
@@ -84,7 +129,6 @@ def set_version(version_string, filename='version.json'):
     data["patch"] = patch
 
     _write_version(data)  # Save the updated version back to the JSON file
-
 
 # Usage
 # Increment the major version
